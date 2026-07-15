49bccae312322183560104b2bb041baf7a52ca1e
agostondunszt <agostondunsztdev@gmail.com>
2026-07-15 13:27:10 +0200

    Basic Filament resources + admin user

---
+++ app/Filament/Resources/ContactMessageResource.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ContactMessageResource\Pages;
+use App\Filament\Resources\ContactMessageResource\RelationManagers;
+use App\Models\ContactMessage;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ContactMessageResource extends Resource
+{
+    protected static ?string $model = ContactMessage::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Érkezett')
+                    ->dateTime('Y.m.d H:i')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Név')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->label('Email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('message')
+                    ->label('Üzenet')
+                    ->limit(50)
+            ])
+            ->defaultSort('created_at', 'desc')
+            ->actions([
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function canCreate(): bool
+    {
+        return false;
+    }
+
+    public static function canEdit(Model $record): bool
+    {
+        return false;
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListContactMessages::route('/'),
+            'create' => Pages\CreateContactMessage::route('/create'),
+            'edit' => Pages\EditContactMessage::route('/{record}/edit'),
+        ];
+    }
+}
