a9746a36054673ec0142ecab7a8ec2b7ab040c7c
agostondunszt <agostondunsztdev@gmail.com>
2026-07-16 08:20:02 +0200

    Mailing setup

---
+++ app/Listeners/SendContactEmail.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ContactFormSubmitted;
+use App\Mail\ContactFormMail;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Mail;
+
+class SendContactEmail implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(ContactFormSubmitted $event): void
+    {
+        // Use the .env variable here
+        $recipient = env('CONTACT_FORM_RECIPIENT', 'default@example.com');
+
+        Mail::to($recipient)->send(new ContactFormMail($event->data));
+    }
+}
