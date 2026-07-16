ca2f2e461e213f93cfd6436c019c00d487406062
agostondunszt <agostondunsztdev@gmail.com>
2026-07-16 10:24:53 +0200

    Better FE support for smaller screens

--- resources/views/components/reference-card.blade.php
+++ resources/views/components/reference-card.blade.php
@@ -1,10 +1,10 @@
 @props(['reference'])
 
-<div class="flex flex-col group cursor-pointer">
+<div class="flex flex-col group w-[366px] max-w-full shrink-0">
     <div class="w-full h-70 overflow-hidden mb-4 bg-gray-100 flex items-center justify-center">
         <img src="{{ asset('storage/' . $reference->image_path) }}" 
              alt="{{ $reference->title }}" 
-             class="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105">
+             class="w-full h-full object-cover">
     </div>
     
     <div class="font-ibmmono text-[11px] tracking-[0.66px] text-[#7C7C84]">
